0c50ab4ad401a8e20e746173cabc474b5cccb6a2
Jiří Helmich <jiri@helmich.cz>
2015-10-08 09:51:03 +0200

    implemented Population Visualizer

--- src/src/main/scala/discovery/components/visualizer/PopulationVisualizer.scala
+++ src/src/main/scala/discovery/components/visualizer/PopulationVisualizer.scala
@@ -1,15 +1,35 @@
 package discovery.components.visualizer
 
+import discovery.components.common.DescriptorChecker
 import discovery.model._
 import discovery.model.components.VisualizerInstance
-import discovery.model.components.descriptor.Descriptor
+import discovery.model.components.descriptor.AskDescriptor
 
 import scala.concurrent.Future
 
-class PopulationVisualizer extends VisualizerInstance {
+class PopulationVisualizer extends VisualizerInstance with DescriptorChecker {
   val portName = "INPUT"
 
-  override def checkPort(port: Port, state: Option[ComponentState], outputDataSample: DataSample): Future[PortCheckResult] = ???
+  private val descriptor = AskDescriptor(
+    """
+      |PREFIX s: <http://schema.org/>
+      |PREFIX dbo: <http://dbpedia.org/ontology/>
+      |
+      |    ASK {
+      |      ?p dbo:populationTotal ?populationCount ;
+      |         s:name ?placeName ;
+      |         s:geo [
+      |           s:latitude ?lat ;
+      |           s:longitude ?lng
+      |         ]
+      |      .
+      |    }
+    """.stripMargin
+  )
 
-  override def getInputPorts: Seq[Port] = ???
+  override def checkPort(port: Port, state: Option[ComponentState], outputDataSample: DataSample): Future[PortCheckResult] = {
+    checkStatelessDescriptors(outputDataSample, descriptor)
+  }
+
+  override def getInputPorts: Seq[Port] = Seq(Port(portName, 0))
 }
